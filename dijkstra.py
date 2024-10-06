9217ad7ee5777679becb96073310eff558e00360
RaulggSouza <raulggsouza@hotmail.com>
2024-10-06 17:06:09 -0300

    continue project later...

--- dijkstra.py
+++ dijkstra.py
@@ -1,19 +1,32 @@
 def dijkstra(graph, source, destination):
     possibleRoutes = []
     unvisited = list(graph.keys())
-    for link in graph.values():
-        for node in link:
-            if node[0] not in unvisited:
-                ...
+    for l in graph[source]:
+        initialLinks = list()
+        initialLinks.append(l)
+        possibleRoutes.append(initialLinks)
+    unvisited.remove(source)
+    
+    for node in unvisited:
+        for link in graph[node]:
+            print(link)
+            # if node[0] in unvisited:
+            #     possibleRoutes[i].append(node)
+            #     if node[0] != destination:
+            #         unvisited.remove(node[0])
+            #     if node == source:
+            #         ...
+            #     ...
         ...
     ...
+    print(possibleRoutes)
 
 def main():
     graph = {'A':[('B',2),('C',3)],
             'B':[('A',2),('D',4)],
             'C':[('A',3),('D',2)],
             'D':[('B',4),('C',2)]}
-    source = 0
-    destination = 3
+    source = 'A'
+    destination = 'D'
     dijkstra(graph, source, destination)
 main()
